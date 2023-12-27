676a5ba424d633a898b5ad2654b1c6940b9c4d89
Sylvan Martin <slm338@cornell.edu>
2023-12-27 00:06:47 -0500

    Implement Queue and Stack

---
+++ Sources/Structures/Queue.swift
@@ -0,0 +1,35 @@
+//
+//  Queue.swift
+//
+//
+//  Created by Sylvan Martin on 12/26/23.
+//
+
+import Foundation
+
+/**
+ * A first in, first out queue
+ */
+class Queue<T: Equatable> {
+    
+    // MARK: Properties
+    
+    private var list: LinkedList<T>
+    
+    /**
+     * The number of items currently in the queue
+     */
+    public var count: Int {
+        list.count
+    }
+    
+    /**
+     * Whether or not the queue is empty
+     */
+    
+    
+    // MARK: Initializers
+    
+    
+    
+}
---
+++ Sources/Structures/Stack.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Sylvan Martin on 12/27/23.
+//
+
+import Foundation
