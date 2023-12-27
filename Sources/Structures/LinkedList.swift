0079a6ef780b7b7936192446c80e6b792ec30972
Sylvan Martin <slm338@cornell.edu>
2023-12-26 23:48:38 -0500

    Implement Linked List

---
+++ Sources/Structures/LinkedList.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Sylvan Martin on 12/26/23.
+//
+
+import Foundation
---
+++ Tests/StructuresTests/LinkedListTests.swift
@@ -0,0 +1,18 @@
+//
+//  LinkedListTests.swift
+//  
+//
+//  Created by Sylvan Martin on 12/26/23.
+//
+
+import XCTest
+import Structures
+
+final class LinkedListTests: XCTestCase {
+
+    func testExample() throws {
+        let ll = LinkedList<Int>()
+        
+    }
+    
+}
